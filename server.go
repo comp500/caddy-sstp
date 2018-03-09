34e5dfe5ea276c0843ab0f98ed3bd0a440fd1c44
comp500 <comp500@users.noreply.github.com>
2018-03-09 20:49:37 +0000

    Do http hijacking

--- server.go
+++ server.go
@@ -27,6 +27,30 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 		}
 
 		fmt.Print("Got a sstp request")
+
+		hijacker, ok := w.(http.Hijacker)
+		if !ok {
+			return http.StatusInternalServerError, errors.New("ResponseWriter does not implement Hijacker")
+		}
+		// Hijack connection
+		// Ignore returned bufio.Reader, as client will only give more data after first response
+		clientConn, _, err := hijacker.Hijack()
+		if err != nil {
+			return http.StatusInternalServerError, errors.New("failed to hijack: " + err.Error())
+		}
+		defer clientConn.Close()
+
+		n, err := fmt.Fprintf(clientConn, "%s\r\n%s\r\n%s\r\n%s\r\n\r\n",
+			"HTTP/1.1 200 OK",
+			"Date: Thu, 09 Nov 2006 00:51:09 GMT",
+			"Server: Microsoft-HTTPAPI/2.0",
+			"Content-Length: 18446744073709551615")
+		if err != nil {
+			return http.StatusInternalServerError, errors.New("Failed to send response")
+		}
+
+		fmt.Printf("Written %v bytes http response", n)
+
 		return 200, nil
 	}
 	fmt.Print("Got a request")
