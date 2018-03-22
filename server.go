c3735e6947bc056861f7f0a55112ea88d0552d27
comp500 <comp500@users.noreply.github.com>
2018-03-22 00:13:12 +0000

    Hook in code from sstp-go

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package sstp
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"strconv"
 
@@ -62,9 +64,111 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 
 			fmt.Printf("Written %v bytes http response", n)
 
+			// Pass the connection to handleConnection
+			go s.handleConnection(clientConn)
+
 			return 0, nil
 		}
 	}
 	fmt.Print("Got a request")
 	return s.NextHandler.ServeHTTP(w, r)
+}
+
+type parseReturn struct {
+	isControl bool
+	Data      []byte
+}
+
+// Handles SSTP connection after HTTP 200 is sent
+func (s Server) handleConnection(c net.Conn) {
+	// Shut down the connection.
+	defer c.Close()
+
+	// TODO: make more idiomatic
+
+	ch := make(chan parseReturn)
+	eCh := make(chan error)
+
+	packChan := make(chan []byte)
+	pppdInstance := pppdInstance{nil, nil, newUnescaper(packetHandler{c, packChan})} // store null pointer to future pppd instance
+
+	// Start a goroutine to read from our net connection
+	go func(ch chan parseReturn, eCh chan error) {
+		for {
+			// try to read the data
+			var data [4]byte
+			n, err := c.Read(data[:])
+			if err != nil {
+				// send an error if it's encountered
+				eCh <- err
+				return
+			}
+			if n < 4 {
+				eCh <- errors.New("Invalid packet")
+				return
+			}
+			isControl, lengthToRead, err := decodeHeader(data[:])
+			if err != nil {
+				// send an error if it's encountered
+				eCh <- err
+				return
+			}
+			newData := make([]byte, lengthToRead)
+			n, err = c.Read(newData)
+			if err != nil {
+				// send an error if it's encountered
+				eCh <- err
+				return
+			}
+			if n != lengthToRead {
+				eCh <- errors.New("Not all of packet read")
+				return
+			}
+			ch <- parseReturn{isControl, newData}
+		}
+	}(ch, eCh)
+
+	go func(packChan chan []byte) {
+		for {
+			select {
+			case data := <-packChan: // This case means we recieved data on the connection
+				c.Write(data)
+			}
+		}
+	}(packChan)
+
+	//ticker := time.Tick(time.Second)
+	// continuously read from the connection
+	for {
+		select {
+		case data := <-ch: // This case means we recieved data on the connection
+			// Do something with the data
+			//log.Printf("%s\n", hex.Dump(data))
+			if data.isControl {
+				header := parseControl(data.Data)
+				handleControlPacket(header, c, &pppdInstance)
+			} else {
+				handleDataPacket(data.Data, c, &pppdInstance)
+			}
+		case err := <-eCh: // This case means we got an error and the goroutine has finished
+			if err == io.EOF {
+				log.Print("Client disconnected")
+				if pppdInstance.commandInst != nil {
+					// kill pppd if disconnect
+					err := pppdInstance.commandInst.Process.Kill()
+					handleErr(err)
+					pppdInstance.commandInst = nil
+				}
+			} else {
+				log.Fatalf("%s\n", err)
+				// handle our error then exit for loop
+				break
+				// This will timeout on the read.
+				//case <-ticker:
+				// do nothing? this is just so we can time out if we need to.
+				// you probably don't even need to have this here unless you want
+				// do something specifically on the timeout.
+			}
+		}
+	}
 }
