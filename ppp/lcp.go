dd3d11e625825f88009de694c4fb4ded0f262546
comp500 <comp500@users.noreply.github.com>
2018-03-24 12:29:00 +0000

    Begin implementation of LCP automaton

---
+++ ppp/lcp.go
@@ -0,0 +1,140 @@
+package ppp
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+)
+
+// controlCode is the LCP/IPCP/CCP control protocol message code of this packet
+type controlCode uint16
+
+// Constants for controlCode values
+// See https://www.iana.org/assignments/ppp-numbers/ppp-numbers.xml for the full list
+// Don't use iota, as each number MUST be equal to given
+const (
+	controlCodeConfigureRequest controlCode = 1
+	controlCodeConfigureAck     controlCode = 2
+	controlCodeConfigureNak     controlCode = 3
+	controlCodeConfigureReject  controlCode = 4
+	controlCodeTerminateRequest controlCode = 5
+	controlCodeTerminateAck     controlCode = 6
+	controlCodeReject           controlCode = 7
+	controlCodeProtocolReject   controlCode = 8
+	controlCodeEchoRequest      controlCode = 9
+	controlCodeEchoReply        controlCode = 10
+	controlCodeDiscardRequest   controlCode = 11
+	// implement Identification and Time-Remaining?
+	// implement CCP?
+)
+
+func (k controlCode) String() string {
+	switch k {
+	case controlCodeConfigureRequest:
+		return "Configure-Request"
+	case controlCodeConfigureAck:
+		return "Configure-Ack"
+	case controlCodeConfigureNak:
+		return "Configure-Nak"
+	case controlCodeConfigureReject:
+		return "Configure-Reject"
+	case controlCodeTerminateRequest:
+		return "Terminate-Request"
+	case controlCodeTerminateAck:
+		return "Terminate-Ack"
+	case controlCodeReject:
+		return "Code-Reject"
+	case controlCodeProtocolReject:
+		return "Protocol-Reject"
+	case controlCodeEchoRequest:
+		return "Echo-Request"
+	case controlCodeEchoReply:
+		return "Echo-Reply"
+	case controlCodeDiscardRequest:
+		return "Discard-Request"
+	default:
+		return fmt.Sprintf("Unknown (%d)", k)
+	}
+}
+
+// lcpOption is a LCP Configuration Option
+type lcpOption uint16
+
+// Constants for lcpOption values
+const (
+	lcpOptionMRU          lcpOption = 1 // Maximum-Recieve-Unit
+	lcpOptionAuthProtocol lcpOption = 3
+	lcpOptionMagicNumber  lcpOption = 4
+	lcpOptionPFC          lcpOption = 7 // Protocol-Field-Compression
+	lcpOptionACFC         lcpOption = 8 // Address-and-Control-Field-Compression
+)
+
+func (k lcpOption) String() string {
+	switch k {
+	case lcpOptionMRU:
+		return "Maximum-Receive-Unit"
+	case lcpOptionAuthProtocol:
+		return "Authentication-Protocol"
+	case lcpOptionMagicNumber:
+		return "Magic-Number"
+	case lcpOptionPFC:
+		return "Protocol-Field-Compression"
+	case lcpOptionACFC:
+		return "Address-and-Control-Field-Compression"
+	default:
+		return fmt.Sprintf("Unknown (%d)", k)
+	}
+}
+
+// lcpState is the current status of the LCP negotiation automaton
+type lcpState int
+
+// Constants for lcpState values
+const (
+	lcpStateInitial lcpState = iota
+	lcpStateStarting
+	lcpStateClosed
+	lcpStateStopped
+	lcpStateClosing
+	lcpStateStopping
+	lcpStateReqSent
+	lcpStateAckReceived
+	lcpStateAckSent
+	lcpStateOpened
+)
+
+func (p *nativeConnection) lcpUp() {
+	if p.lcpState == lcpStateInitial {
+		p.lcpState = lcpStateClosed
+	}
+}
+
+func (p *nativeConnection) lcpDown() {
+
+}
+
+func (p *nativeConnection) lcpOpen() {
+
+}
+
+func (p *nativeConnection) lcpClose() {
+
+}
+
+func (p *nativeConnection) parseLCP(data []byte) error {
+	controlCode := controlCode(data[0])
+	// TODO implement identifier?
+	packetLength := binary.BigEndian.Uint16(data[2:4])
+
+	// Shift data along, trim to packetLength
+	data = data[4:packetLength]
+
+	switch controlCode {
+	case controlCodeConfigureRequest:
+		log.Printf("It's a Configure-Request")
+	default:
+		log.Printf("%s not implemented", controlCode)
+	}
+
+	return nil
+}
--- ppp/native.go
+++ ppp/native.go
@@ -16,13 +16,14 @@ type nativeConnection struct {
 	hasBeenClosed  bool
 	acfcApplied    bool // Indicates whether Address-and-Control-Field-Compression is applied
 	pfcApplied     bool // Indicates whether Protocol-Field-Compression is applied
+	lcpState       lcpState
 }
 
 func (p *nativeConnection) Write(data []byte) (int, error) {
 	if p.hasBeenClosed {
 		return 0, errors.New("ppp write after close")
 	}
-	err := parsePPP(data, p)
+	err := p.parsePPP(data)
 	if err != nil {
 		return 0, err
 	}
@@ -91,7 +92,7 @@ func (k protocolType) String() string {
 	}
 }
 
-func parsePPP(data []byte, p *nativeConnection) error {
+func (p *nativeConnection) parsePPP(data []byte) error {
 	// TODO: parse packets for *every* protocol
 	if !p.acfcApplied {
 		// If address and control field is not compressed, remove it
@@ -120,7 +121,7 @@ func parsePPP(data []byte, p *nativeConnection) error {
 	if p.linkStatus == linkStatusEstablish {
 		if protocolNumber == protocolTypeLCP {
 			log.Print("LCP")
-			return parseLCP(data, p)
+			return p.parseLCP(data)
 		}
 		log.Print("Discarding packet")
 		// silently discard, only allow LCP
@@ -130,7 +131,7 @@ func parsePPP(data []byte, p *nativeConnection) error {
 		switch protocolNumber {
 		case protocolTypeLCP:
 			log.Print("LCP")
-			return parseLCP(data, p)
+			return p.parseLCP(data)
 		case protocolTypePAP:
 			log.Print("PAP")
 		case protocolTypeCHAP:
@@ -151,7 +152,7 @@ func parsePPP(data []byte, p *nativeConnection) error {
 			// silently discard
 		case protocolTypeLCP:
 			log.Print("LCP")
-			return parseLCP(data, p)
+			return p.parseLCP(data)
 		case protocolTypeIPCP:
 			log.Print("IPCP")
 		case protocolTypeCCP:
@@ -165,108 +166,10 @@ func parsePPP(data []byte, p *nativeConnection) error {
 	if p.linkStatus == linkStatusTerminate {
 		if protocolNumber == protocolTypeLCP {
 			log.Print("LCP")
-			return parseLCP(data, p)
+			return p.parseLCP(data)
 		}
 		log.Print("Discarding packet")
 		// silently discard, only allow LCP
-	}
-
-	return nil
-}
-
-// controlCode is the LCP/IPCP/CCP control protocol message code of this packet
-type controlCode uint16
-
-// Constants for controlCode values
-// See https://www.iana.org/assignments/ppp-numbers/ppp-numbers.xml for the full list
-// Don't use iota, as each number MUST be equal to given
-const (
-	controlCodeConfigureRequest controlCode = 1
-	controlCodeConfigureAck     controlCode = 2
-	controlCodeConfigureNak     controlCode = 3
-	controlCodeConfigureReject  controlCode = 4
-	controlCodeTerminateRequest controlCode = 5
-	controlCodeTerminateAck     controlCode = 6
-	controlCodeReject           controlCode = 7
-	controlCodeProtocolReject   controlCode = 8
-	controlCodeEchoRequest      controlCode = 9
-	controlCodeEchoReply        controlCode = 10
-	controlCodeDiscardRequest   controlCode = 11
-	// implement Identification and Time-Remaining?
-	// implement CCP?
-)
-
-func (k controlCode) String() string {
-	switch k {
-	case controlCodeConfigureRequest:
-		return "Configure-Request"
-	case controlCodeConfigureAck:
-		return "Configure-Ack"
-	case controlCodeConfigureNak:
-		return "Configure-Nak"
-	case controlCodeConfigureReject:
-		return "Configure-Reject"
-	case controlCodeTerminateRequest:
-		return "Terminate-Request"
-	case controlCodeTerminateAck:
-		return "Terminate-Ack"
-	case controlCodeReject:
-		return "Code-Reject"
-	case controlCodeProtocolReject:
-		return "Protocol-Reject"
-	case controlCodeEchoRequest:
-		return "Echo-Request"
-	case controlCodeEchoReply:
-		return "Echo-Reply"
-	case controlCodeDiscardRequest:
-		return "Discard-Request"
-	default:
-		return fmt.Sprintf("Unknown (%d)", k)
-	}
-}
-
-// lcpOption is a LCP Configuration Option
-type lcpOption uint16
-
-// Constants for lcpOption values
-const (
-	lcpOptionMRU          lcpOption = 1 // Maximum-Recieve-Unit
-	lcpOptionAuthProtocol lcpOption = 3
-	lcpOptionMagicNumber  lcpOption = 4
-	lcpOptionPFC          lcpOption = 7 // Protocol-Field-Compression
-	lcpOptionACFC         lcpOption = 8 // Address-and-Control-Field-Compression
-)
-
-func (k lcpOption) String() string {
-	switch k {
-	case lcpOptionMRU:
-		return "Maximum-Receive-Unit"
-	case lcpOptionAuthProtocol:
-		return "Authentication-Protocol"
-	case lcpOptionMagicNumber:
-		return "Magic-Number"
-	case lcpOptionPFC:
-		return "Protocol-Field-Compression"
-	case lcpOptionACFC:
-		return "Address-and-Control-Field-Compression"
-	default:
-		return fmt.Sprintf("Unknown (%d)", k)
-	}
-}
-
-func parseLCP(data []byte, p *nativeConnection) error {
-	controlCode := controlCode(data[0])
-	// TODO implement identifier?
-	packetLength := binary.BigEndian.Uint16(data[2:4])
-
-	// Shift data along, trim to packetLength
-	data = data[4:packetLength]
-
-	switch controlCode {
-	case controlCodeConfigureRequest:
-		log.Printf("It's a Configure-Request")
-	default:
-		log.Printf("%s not implemented", controlCode)
 	}
 
 	return nil
